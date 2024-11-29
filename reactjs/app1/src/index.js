c10baae62e6a000121ad5a1b7cae07e903305642
theeasylearn <theeasylearn@gmail.com>
2024-11-29 17:04:33 +0530

    day 3

--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+var page = <div><h1>Hello friends</h1>Rahul Vedant Karan Hadi etc</div>;
+root.render(page);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
