88232f8d5830573b819da14a26cffcd37dea7ae0
theeasylearn <theeasylearn@gmail.com>
2024-12-01 19:02:37 +0530

    bootstrap day 2

---
+++ reactjs/app1/src/3_bootstrap_1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+var page = (<div className='container'>
+    <div className='row'>
+        <div className='col-12'>
+            <h1 className='text-primary'>Working with bootstrap</h1>
+
+        </div>
+    </div>
+</div>);
+root.render(page);
+
