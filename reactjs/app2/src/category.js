72763e1c1e24f963eb462aef48cdf4916c085c56
theeasylearn <theeasylearn@gmail.com>
2025-01-22 17:06:28 +0530

    api calling

--- reactjs/app2/src/category.js
+++ reactjs/app2/src/category.js
@@ -1,30 +1,54 @@
 import { useEffect, useState } from "react";
 import Menu from "./menu";
 import { Link } from "react-router-dom";
+import $ from "jquery";
+import "datatables.net";
+import { getBase, getImageBase } from "./common";
 export default function Category() {
   //create state array 
   let [categories, setCategory] = useState([]);
+
   //now get data from server
   useEffect(() => {
     if (categories.length === 0) {
-      let apiAddress = "https://theeasylearnacademy.com/shop/ws/category.php";
+      let apiAddress = getBase() + "category.php";
       fetch(apiAddress).then((response) => response.json()).then((data) => {
         console.log(data);
-        setCategory(data);
-      }).catch((error) => console.log(error));
+        let error = data[0]['error'];
+        if(error !== 'no')
+            alert(error)
+          else 
+          {
+              let total = data[1]['total'];
+              if(total === 0)
+                alert('no category found');
+              else 
+              {
+                //delete 2 object from beginning 
+                data.splice(0,2);
+                setCategory(data);
+              }
+          }
+      }).catch((error) => {
+          alert(error);
+      });
+    }
+    else 
+    {
+        $("#myTable").DataTable();
     }
   });
-  let Display = function () {
+  
+  let Display = function (item) {
     return (<tr>
-      <td>1</td>
-      <td>Sample Title</td>
-      <td>Sample Detail</td>
+      <td>{item.id}</td>
+      <td>{item.title}</td>
       <td>
-        <a href="https://picsum.photos/1000" data-fancybox="gallery" data-caption="Sample Title">
-          <img src="https://picsum.photos/100" alt="Photo" style={{ "max-width": "100px" }} />
+        <a href={getImageBase() + "category/" + item.photo} data-fancybox="gallery" data-caption="Sample Title">
+          <img src={getImageBase() + "category/" + item.photo} alt="Photo" style={{ "max-width": "100px" }} />
         </a>
       </td>
-      <td>Yes</td>
+      <td>{(item.islive === '1') ? "Yes":"No"}</td>
       <td>
         <Link className="btn btn-warning btn-sm" to="/category/edit">
           <i className="fas fa-edit" /> Edit
@@ -58,19 +82,18 @@ export default function Category() {
                   <Link to="/category/add" className="btn btn-primary">Add Category</Link>
                 </div>
                 <div className="card-body">
-                  <table id="myTable" className="table table-striped">
+                  <table id="myTable" className="table table-striped" width='100%'>
                     <thead>
                       <tr>
                         <th scope="col">ID</th>
                         <th scope="col">Title</th>
-                        <th scope="col">Detail</th>
                         <th scope="col">Photo</th>
                         <th scope="col">Is Live</th>
                         <th scope="col">Actions</th>
                       </tr>
                     </thead>
                     <tbody>
-                      {categories.map((item) => Display())}
+                      {categories.map((item) => Display(item))}
                     </tbody>
                   </table>
                 </div>
---
+++ reactjs/app2/src/common.js
@@ -0,0 +1,9 @@
+const BASE = "https://theeasylearnacademy.com/shop/";
+export function getBase()
+{
+    return BASE + "ws/";
+}
+export function getImageBase()
+{
+    return BASE + "images/";
+}
