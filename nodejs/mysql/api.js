8fe5ef428fa19943cde4428b0ccfbc8afc516bd2
theeasylearn <theeasylearn@gmail.com>
2025-03-24 16:30:44 +0530

    api.js

--- nodejs/mysql/api.js
+++ nodejs/mysql/api.js
@@ -5,11 +5,35 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const PERSON = "/person";
-//fetch products 
-//localhost:5000/product 
+const SUBJECT = "/subject";
+
+//fetch all person  
+//localhost:5000/person
 //method get 
+
+//fetch one person  
+//localhost:5000/person?id=2
+//method get 
+
+//fetch one male person  
+//localhost:5000/person?gender=M
+//method get 
+
+//fetch one male person  
+//localhost:5000/person?gender=F
+//method get 
+
 app.get(PERSON, function (request, response) {
-    let sql = "select * from person order by id desc";
+
+    let id = request.query.id; 
+    let gender = request.query.gender; 
+    let sql;
+    if(id !== undefined)
+        sql = `select * from person where id=${id}`;
+    else if(gender !== undefined)
+        sql = `select * from person where gender='${gender}' order by id desc`;
+    else 
+        sql = "select * from person order by id desc";
     //run sql command
     connection.con.query(sql, function (error, result) {
         if (error)
@@ -22,8 +46,8 @@ app.get(PERSON, function (request, response) {
     });
 });
 
-//insert product 
-//localhost:5000/product 
+//insert new person  
+//localhost:5000/person 
 //method POST 
 app.post(PERSON, function (request, response) {
     var { fullname, gender, dob } = request.body;
@@ -43,5 +67,112 @@ app.post(PERSON, function (request, response) {
     }
 });
 
+//delete person 
+//localhost:5000/person  
+//method delete
+app.delete(PERSON, function (request, response) {
+    var id = request.body.id;
+    if (id === undefined) {
+        response.json([{ 'error': 'input is missing, id required' }]);
+    }
+    else {
+        let sql = `delete from person where id=${id}`;
+        connection.con.query(sql, function (error, result) {
+            if (error)
+                response.json([{ 'error': 'oops something went wrong contact developer' }]);
+            else {
+                response.json([{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'person deleted' }]);
+            }
+        });
+    }
+});
+
+//update person 
+//localhost:5000/person  
+//method put
+app.put(PERSON, function (request, response) {
+    //object destructing 
+    var { id, fullname, gender, dob, salary } = request.body;
+    //input validation
+    if (fullname === undefined || gender === undefined || dob === undefined || salary === undefined || id === undefined) {
+        response.json([{ 'error': 'input missing, id fullname gender dob salary is required' }]);
+    }
+    else 
+    {
+        let sql = `update person set fullname='${fullname}', gender='${gender}',dob='${dob}',salary='${salary}' where id='${id}'`;
+        connection.con.query(sql, function (error, result) {
+            if (error)
+                response.json([{ 'error': 'oops something went wrong contact developer' }]);
+            else {
+                response.json([{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'person updated' }]);
+            }
+        });
+    }
+});
+app.get(SUBJECT, (req, res) => {
+    let id = req.query.id;
+    let sql = id ? `SELECT * FROM subject WHERE id=${id}` : `SELECT * FROM subject ORDER BY id DESC`;
+    
+    connection.con.query(sql, (error, result) => {
+        if (error) {
+            res.json([{ 'error': 'oops something went wrong, contact developer' }]);
+        } else {
+            result.unshift({ total: result.length });
+            result.unshift({ error: 'no' });
+            res.json(result);
+        }
+    });
+});
+
+// Insert a new subject
+app.post(SUBJECT, (req, res) => {
+    const { name, description } = req.body;
+    if (!name || !description) {
+        return res.json([{ 'error': 'input missing, name and description are required' }]);
+    }
+    
+    let sql = `INSERT INTO subject (name, description) VALUES ('${name}', '${description}')`;
+    connection.con.query(sql, (error, result) => {
+        if (error) {
+            res.json([{ 'error': 'oops something went wrong, contact developer' }]);
+        } else {
+            res.json([{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'subject inserted' }]);
+        }
+    });
+});
+
+// Update a subject
+app.put(SUBJECT, (req, res) => {
+    const { id, name, description } = req.body;
+    if (!id || !name || !description) {
+        return res.json([{ 'error': 'input missing, id, name, and description are required' }]);
+    }
+    
+    let sql = `UPDATE subject SET name='${name}', description='${description}' WHERE id=${id}`;
+    connection.con.query(sql, (error, result) => {
+        if (error) {
+            res.json([{ 'error': 'oops something went wrong, contact developer' }]);
+        } else {
+            res.json([{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'subject updated' }]);
+        }
+    });
+});
+
+// Delete a subject
+app.delete(SUBJECT, (req, res) => {
+    const { id } = req.body;
+    if (!id) {
+        return res.json([{ 'error': 'input missing, id is required' }]);
+    }
+    
+    let sql = `DELETE FROM subject WHERE id=${id}`;
+    connection.con.query(sql, (error, result) => {
+        if (error) {
+            res.json([{ 'error': 'oops something went wrong, contact developer' }]);
+        } else {
+            res.json([{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'subject deleted' }]);
+        }
+    });
+});
 app.listen(5000);
 console.log('ready to accept request');
